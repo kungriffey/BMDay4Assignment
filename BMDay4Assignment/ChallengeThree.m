538ccef553b005f43d83915e865f9e622f79486d
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-09 17:04:28 -0400

    committed challenge 3

---
+++ BMDay4Assignment/ChallengeThree.m
@@ -0,0 +1,18 @@
+//
+//  ChallengeThree.m
+//  BMDay4Assignment
+//
+//  Created by Kunwardeep Gill on 2015-04-09.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*
+
+ [car getGas]
+ [boat getGas]
+ [car steerTheWheel]
+ [boat steerTheWheel]
+ 
+*/
